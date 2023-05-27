eed84c6cf8af09b7371b358d06b9a8c4da8b2def
Antonio <thiago.sousa@rededor.com.br>
2023-05-27 17:44:07 -0300

    chore: add express and cors and settings of server

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,9 +1,15 @@
-class AppServer {
-  private app: string
+import express from 'express'
+import 'express-async-errors'
+import cors from 'cors'
 
-  constructor(info: string) {
-    this.app = info ?? 'Olá Dev'
-  }
-}
-const obj = new AppServer(null)
-console.log(obj)
+const app = express()
+
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  return res.json({ msg: 'Olá Devs' })
+})
+
+app.listen(8089, () => console.log(`Server started on port ${8089} ⭐`))
