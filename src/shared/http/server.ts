e4b68782c2b86f820c526750a63bd776354a4193
Antonio Thiago <noplex.dev@gmail.com>
2023-05-28 14:51:20 -0300

    feat: add swagger-ui-express

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,14 +2,17 @@ import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
 import 'dotenv/config'
+import swaggerUi from 'swagger-ui-express'
 import { routes } from './routes/routes'
 import { AppError } from '@shared/errors/AppError'
+import swaggerFile from '../../swagger.json'
 
 const app = express()
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(routes)
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
